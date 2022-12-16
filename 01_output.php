e9764c046acc09efa3ed0eec8f97d71ee5f2e62e
RutvikMori18 <rutvikmori123@gmail.com>
2022-12-16 11:45:10 +0530

    -print statement in php

--- 01_output.php
+++ 01_output.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Hello goood morninng';
+//echo used for print statements
+echo 123,'hello world',10.5;
+
+//echo used for print statements
+print_r([1,2,3,4]);
+
+
+//var_dump() used for the more info of variable nad data type and length
+var_dump("Hello world");
+
+
